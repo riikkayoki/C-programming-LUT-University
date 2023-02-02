8aecd1567859bd7ab7d7fc43d84bf82d515bded7
riikkayoki <74421508+riikkayoki@users.noreply.github.com>
2023-02-02 21:07:58 +0200

    first commit

---
+++ practice_exercises/E1_1.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(void) {
+printf("Minä tein tämän!\n");
+return(0);
+}
---
+++ practice_exercises/E1_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) {
+/* Tämä on kommentti */
+printf("Minä tein tämän!\n");
+printf("Huomaa, että teksti jatkuu aina edellisen ");
+printf("perään,\n ellei tulostus pääty rivinvaihtomerkkiin!");
+/* C-kielen kommenttimerkit
+ovat monirivisiä */
+return(0);
+}
